14d0a0aee77df9e04fed7c13a271becb11e3d5fa
Aman Verma <amanv@iitrpr.ac.in>
2022-03-23 22:02:08 +0530

    Time: 1 ms, Memory: 40.8 MB - LeetHub

---
+++ fibonacci-number/fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    HashMap<Integer, Integer> cache = new HashMap<>();
+    public int fib(int n) {
+        if(n<2){
+            return n;
+        }
+        if(cache.containsKey(n)){
+            return cache.get(n);
+        }
+        int val = fib(n-1)+fib(n-2);
+        cache.put(n, val);
+        return val;
+        
+    }
+}
